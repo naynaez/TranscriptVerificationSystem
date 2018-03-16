c90d72ace08f7c2e55c0452ed968e3415b5356a1
Peerawat Pipattanakulchai <peerawatpipattanakulchai@Peerawats-MacBook-Pro.local>
2018-03-16 19:36:50 +0700

    Task: Implement Fuction , Improove Web pages

--- dappDemo/client/main.js
+++ dappDemo/client/main.js
@@ -6,34 +6,42 @@ import './main.html';
 console.log("Hello");
 var primary = "0x31026b17b78dc930cfb57232f647f7ac97520374"  // Account in Localhost
 console.log(web3.fromWei(web3.eth.getBalance(primary)).c,"ether");
-
+console.log(getOrigTranscriptHash("0x6425f318885746ab14577bce87f8e38a2afedb9436ab4c6f0c6161933b341fa8"));
 
 ///////////////////// XXX Unlock Account  ////////////////////
-var accountToUnlock = "0x93878957f62e21dd9cb63c760a6e98325a2ea17e";
-var passPhrase = "THE SECOND TEST TO GET NEW ACCOUNT";
-//web3.personal.unlockAccount(accountToUnlock, passPhrase, 300);
+// var accountToUnlock = "0x93878957f62e21dd9cb63c760a6e98325a2ea17e";
+// var passPhrase = "THE SECOND TEST TO GET NEW ACCOUNT";
+function unlockAccount(accountToUnlock,passPhrase){
+  web3.personal.unlockAccount(accountToUnlock, passPhrase, 300);
+}
 
 
 ///////////////////// Create Transaction //////////////////
 //data 36 byte
-var regTranscriptHash = "0x74657374206372656174696e67207472616e73616374696f6e20";
-var universityPublicKey = "0x93878957f62e21dd9cb63c760a6e98325a2ea17e";
-// txHash = web3.eth.sendTransaction(
-//   {
-//     from: universityPublicKey,
-//     value: 0,
-//     gas: 100000,
-//     gasPrice: 0,
-//     data: regTranscriptHash
-//   }
-// );
-//console.log(txHash);
+//var regTranscriptHash = "0x74657374206372656174696e67207472616e73616374696f6e20";
+//var universityPublicKey = "0x93878957f62e21dd9cb63c760a6e98325a2ea17e";
+function createTransaction(regTranscriptHash,universityPublicKey){
+  txHash = web3.eth.sendTransaction(
+    {
+      from: universityPublicKey,
+      value: 0,
+      gas: 100000,
+      gasPrice: 0,
+      data: regTranscriptHash
+    }
+  );
+  return txHash;
+}
 
 
 /////////////// Get data from transaction //////////////////
-var txHashForQuery = "0x6425f318885746ab14577bce87f8e38a2afedb9436ab4c6f0c6161933b341fa8";
-var transaction = web3.eth.getTransaction(txHashForQuery);
-console.log(transaction.input);
+function getOrigTranscriptHash(txHashForQuery){
+  var transaction = web3.eth.getTransaction(txHashForQuery);
+  var origTranscriptHash = transaction.input;
+  return origTranscriptHash;
+}
+
+
 
 
 
